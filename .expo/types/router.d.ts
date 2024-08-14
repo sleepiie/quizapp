2be9b29994da56e5fe8e4d8c1029fd3ac79eeee1
sleepiie <122366817+sleepiie@users.noreply.github.com>
2024-08-14 22:45:53 +0700

    remove unwanted file

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/explore` | `/_sitemap` | `/explore` | `/mainscreen` | `/quizscreen` | `/txtfile/questionapp`;
+      StaticRoutes: `/_sitemap` | `/logic` | `/mainscreen` | `/quizscreen` | `/txtfile/questionapp`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
